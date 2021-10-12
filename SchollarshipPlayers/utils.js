315d40974a72074dec6945cab6a2aa8df9b8ad82
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2021-10-12 20:48:59 -0300

    Add verifyIfExists feature

--- SchollarshipPlayers/utils.js
+++ SchollarshipPlayers/utils.js
@@ -1,8 +1,15 @@
-function normalizeNaN(number){
-    parsedNumber = parseInt(number)
-    if(isNaN(parsedNumber)){
-      return 0;
-    }else{
-      return parsedNumber;
-    }
+function normalizeNaN(number) {
+  parsedNumber = parseInt(number)
+  if (isNaN(parsedNumber)) {
+    return 0;
+  } else {
+    return parsedNumber;
   }
+}
+function verifyIfExists(fullString, compareString) {
+  if (fullString.indexOf(compareString) == -1) {
+    return false;
+  } else {
+    return true;
+  }
+}
