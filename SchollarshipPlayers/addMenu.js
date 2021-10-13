8646666f27586bacdd2439559f7155088cfa2e3a
Juan Colchete <38388534+juanudk@users.noreply.github.com>
2021-10-12 21:36:30 -0300

    Add Get weather estate to menu

--- SchollarshipPlayers/addMenu.js
+++ SchollarshipPlayers/addMenu.js
@@ -1,10 +1,13 @@
 function createMenu() {
     SpreadsheetApp.getUi().createMenu("Crypto")
      .addItem("Mount Ranking", "getRanking")
+     .addItem("Get weather estate", "getWeatherEstate")
      .addToUi();
  }
  
  function getRanking() {
    moutRanking();
  }
- 
+ function getWeatherEstate(){
+  fillGreenHouse();
+ }
